baac69262a3e31c198a1e950a675cc1b63a46fb9
annejunkes <anneburkhardtjunkes@gmail.com>
2024-04-24 15:01:24 -0300

    edição no main

--- MaisSorriso/src/ifsc/Cliente.java
+++ MaisSorriso/src/ifsc/Cliente.java
@@ -2,8 +2,50 @@ package ifsc;
 
 public class Cliente {
 	
-	String nome;
-	String codigo;
-	String telefone;
+	private String nome;
+	private String sobrenome;
+	private String cpf;
+	private String endereco;
+	private String codigo;
+	private String telefone;
+	
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
 
+
+	
 }
--- MaisSorriso/src/ifsc/Main.java
+++ MaisSorriso/src/ifsc/Main.java
@@ -1,9 +1,55 @@
 package ifsc;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		ArrayList<Cliente> consultas = new ArrayList<>();
+		
+		int op = 1;
+		switch(op) {
+		
+		case 1:
+			
+			System.out.println("-Cadastro do Cliente-");
+			Cliente cadastro = new Cliente();
+			
+			cadastro.setNome(null);
+			cadastro.setCodigo(null);
+			cadastro.setTelefone(null);
+			cadastro.setEndereco(null);
+			cadastro.setCpf(null);
+			
+			
+			consultas.add(null);
+			break;
+			
+		case 2:
+			
+			System.out.println("-Consultas agendadas-");
+			//codigo - jaque
+			break;
+			
+		case 3:
+			
+			System.out.println("-Buscar consulta-");
+			//codigo - anne
+			break;
+			
+		case 4:
+			
+			System.out.println("-Atualizar cadastro-");
+			//codigo - nao definido
+			break;
+			
+		case 5:
+			System.out.println("-Exclusao de cadastro-");
+			//codigo - nao definido
+			break;
+		}
+		
 
 	}
 
