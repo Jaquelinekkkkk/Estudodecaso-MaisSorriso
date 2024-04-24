d2760fb600e69b300f225d0288d0e527b01210c0
Jaqueline <jaqueline.c2004@aluno.ifsc.edu.br>
2024-04-24 15:13:40 -0300

    Consulta

---
+++ MaisSorriso/src/ifsc/Consulta.java
@@ -0,0 +1,47 @@
+package ifsc;
+
+import java.time.LocalDateTime;
+
+public class Consulta {
+	
+	private String dentitsa;
+	private String auxiliar;
+	private int precoConsulta;
+	private String tipoConsulta;
+	private LocalDateTime dataHora;
+	
+	public String getDentitsa() {
+		return dentitsa;
+	}
+	public void setDentitsa(String dentitsa) {
+		this.dentitsa = dentitsa;
+	}
+	public String getAuxiliar() {
+		return auxiliar;
+	}
+	public void setAuxiliar(String auxiliar) {
+		this.auxiliar = auxiliar;
+	}
+	public int getPrecoConsulta() {
+		return precoConsulta;
+	}
+	public void setPrecoConsulta(int precoConsulta) {
+		this.precoConsulta = precoConsulta;
+	}
+	public String getTipoConsulta() {
+		return tipoConsulta;
+	}
+	public void setTipoConsulta(String tipoConsulta) {
+		this.tipoConsulta = tipoConsulta;
+	}
+	public LocalDateTime getDataHora() {
+		return dataHora;
+	}
+	public void setDataHora(LocalDateTime dataHora) {
+		this.dataHora = dataHora;
+	}
+
+	
+
+
+}
