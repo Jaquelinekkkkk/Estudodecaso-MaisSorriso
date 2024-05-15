f3a898278e6b20dd1310967480618f39dba785ac
jaque <jaqueline.c2004@aluno.ifsc.edu.br>
2024-05-15 14:12:54 -0300

    SINGLETON

--- MaisSorriso/src/controle/ClienteDAO.java
+++ MaisSorriso/src/controle/ClienteDAO.java
@@ -8,8 +8,17 @@ public class ClienteDAO {
 	
 	private ArrayList<Cliente> listaClientes;
 	private int geradorId;
+	private static ClienteDAO  instancia;
 	
-	public ClienteDAO() {
+	public static ClienteDAO getInstancia() {
+		if(instancia == null) {
+			instancia = new ClienteDAO();
+		}
+		return instancia;
+		
+	}
+	//Pseudo metodo*
+	private ClienteDAO() {
 		this.listaClientes = new ArrayList<>();
 		this.geradorId = 0;
 	}
